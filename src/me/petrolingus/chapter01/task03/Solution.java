4e3e74171e10a3b58734a47fc48ffa6006dc7e74
petrolingus <petrolingus@gmail.com>
2021-04-02 01:54:28 +0300

    added suppress warning annotation ManualMinMaxCalculation of task03 in chapter 01

--- src/me/petrolingus/chapter01/task03/Solution.java
+++ src/me/petrolingus/chapter01/task03/Solution.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * @author Lebedev Aleksandr
  *
  */
+@SuppressWarnings("ManualMinMaxCalculation")
 public class Solution {
 
     public static void main(String[] args) {
